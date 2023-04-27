391376c0ab40b9b8f64369a93009affb57f60673
LAPTOP-E9EA0F2K\Kamazawa Ai <ak.01520119@gmail.com>
2023-04-27 09:21:25 +0900

    postsテーブル作成

--- database/migrations/2019_12_24_090209_create_posts_table.php
+++ database/migrations/2019_12_24_090209_create_posts_table.php
@@ -16,8 +16,9 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id')->autoIncrement();
             $table->integer('user_id');
-            $table->string('posts',500);
-            $table->timestamps();
+            $table->varchar('post',400);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
